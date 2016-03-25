f2ad1f7fce2d2fa4e78ae1e667dc1c649817b075
Adrian Micayabas <deepspace30@gmail.com>
2016-03-25 10:55:07 -0400

    ad data retrieval functional

--- AdServiceTest/Controllers/HomeController.cs
+++ AdServiceTest/Controllers/HomeController.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization; 
 
 namespace AdServiceTest.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
-
+        public string GetData() { 
+            WCFAdDataService.AdDataServiceClient adsc = new WCFAdDataService.AdDataServiceClient();
+            WCFAdDataService.Ad[] adData = adsc.GetAdDataByDateRange(new DateTime(2011, 1, 1), new DateTime(2011, 4, 1));
+            JavaScriptSerializer jss = new JavaScriptSerializer();
+            string data = jss.Serialize(adData);
+            return data;
+        }
+        public DateTime GetDate()
+        {
+            return DateTime.Today;
+        }
         public ActionResult Index()
         {
             return View();
--- AdServiceTest/Views/Home/Index.aspx
+++ AdServiceTest/Views/Home/Index.aspx
@@ -6,10 +6,24 @@
 <head runat="server">
     <meta name="viewport" content="width=device-width" />
     <title>Home</title>
+
+    <script type="text/javascript" src="../../Scripts/jquery-1.8.2.min.js"></script>
 </head>
 <body>
-    <div>
+    <div id="data">
         Test
     </div>
+
+    vccv 
+<script>
+    $(document).ready(function () {
+        var dateUrl = "/Home/GetData";
+        $.get(dateUrl, null, function (data) {
+            var adData = jQuery.parseJSON(data);
+            $("#data").html(data);
+        })
+    });
+
+</script>
 </body>
 </html>
